ee10466d1bebd763e3e4df2977526b50f8cce921
Sergoman4 <28062002sergey@gmail.com>
2024-09-29 21:59:27 +1000

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,8 @@
 def personal_sum(numbers):
     result = 0
     incorrect_data = 0
-
     if isinstance(numbers, str):
         numbers = numbers.split(',')
-
     if isinstance(numbers, (list, tuple, set)):
         for number in numbers:
             try:
@@ -16,16 +14,16 @@ def personal_sum(numbers):
     else:
         incorrect_data += 1
         print(f"Некорректный тип данных для подсчёта суммы - {numbers}")
-
     return result, incorrect_data
 
 
 def calculate_average(numbers):
-
     try:
         if not isinstance(numbers, (list, tuple, set)):
             raise TypeError
         total_sum, incorrect_count = personal_sum(numbers)
+        if incorrect_count > 0:
+            return None
         average = total_sum / len(numbers)
         return average
     except ZeroDivisionError:
@@ -34,7 +32,8 @@ def calculate_average(numbers):
         print('В numbers записан некорректный тип данных')
         return None
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}')
+
+print(f'Результат 1: {calculate_average("1, 2, 3, , , , 2, , , 3")}')
 print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
 print(f'Результат 3: {calculate_average(567)}')
 print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
